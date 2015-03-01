f9ed72832ca25c8bf8b59ffefbef068f2fe94152
Eliot Hedeman <ehedeman@barracuda.com>
2015-02-28 21:04:28 -0500

    map now grows as needed

--- hashmap/map.go
+++ hashmap/map.go
@@ -1,22 +1,116 @@
 package hashmap
 
-import "github.com/eliothedeman/structures/list"
-
 import (
-	"sync/atomic"
+	"hash"
+	"hash/fnv"
+
+	"github.com/eliothedeman/structures/list"
 )
 
 // Map an implimentation of a hashmap
 type Map struct {
-	index []*list.LinkedList
-	size  int64
+	index    []*list.LinkedList
+	size     int
+	capasity int
+	hashFunc hash.Hash32
+}
+
+type ListElement struct {
+	key string
+	val interface{}
+}
+
+func NewMap() *Map {
+	return &Map{
+		index:    make([]*list.LinkedList, 32),
+		capasity: 32,
+		hashFunc: fnv.New32(),
+	}
 }
 
 // hash perform a hash function on the key, and return the index it corrosponds to
-func (m *Map) hash(key string) int {
+func (m *Map) hash(key string, max int) int {
+	m.hashFunc.Reset()
+	m.hashFunc.Write([]byte(key))
+	return int(m.hashFunc.Sum32()) % max
+
 }
 
-// Size atomically return the size of the map it it's current state
-func (m *Map) Size() int64 {
-	return atomic.LoadInt64(&m.size)
+// grow double the capasity of the map
+func (m *Map) grow() {
+	m.capasity = m.capasity << 2
+	old := m.index
+	m.index = make([]*list.LinkedList, m.capasity)
+
+	// for every entry that is not nil, copy it's contents to the new index
+	var l *list.LinkedList
+	for i := 0; i < len(old); i++ {
+		l = old[i]
+
+		if l != nil {
+			// copy every element in the list
+			l.Reset()
+			node := l.Next()
+			var le *ListElement
+			for node != nil {
+				le = node.(*ListElement)
+				m.Put(le.key, le.val)
+
+				node = l.Next()
+			}
+		}
+	}
+}
+
+// Put write a key/value pair to the map
+func (m *Map) Put(key string, value interface{}) {
+
+	le := &ListElement{
+		key: key,
+		val: value,
+	}
+
+	i := m.hash(key, m.capasity)
+	li := m.index[i]
+	if li == nil {
+		li = &list.LinkedList{}
+		m.index[i] = li
+	}
+	li.Reset()
+	node := li.Next()
+	for node != nil {
+		if node.(*ListElement).key == key {
+			li.Delete(node)
+		}
+
+		node = li.Next()
+	}
+	m.size++
+	li.Push(le)
+
+	if m.size > m.capasity {
+		m.grow()
+	}
+}
+
+// Get retrieve a value given a key
+func (m *Map) Get(key string) interface{} {
+	i := m.hash(key, m.capasity)
+	li := m.index[i]
+	if li == nil {
+		return nil
+	}
+
+	li.Reset()
+	node := li.Next()
+
+	for node != nil {
+		le := node.(*ListElement)
+		if le.key == key {
+			return le.val
+		}
+
+		node = li.Next()
+	}
+	return nil
 }
---
+++ hashmap/map_test.go
@@ -0,0 +1,57 @@
+package hashmap
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHash(t *testing.T) {
+	m := NewMap()
+	h1 := m.hash("hello", 1024)
+	h2 := m.hash("hello", 1024)
+	if h1 != h2 {
+		t.Fail()
+	}
+}
+
+func TestGet(t *testing.T) {
+	m := NewMap()
+
+	for i := 0; i < 100; i++ {
+		k := fmt.Sprintf("%d", i)
+		m.Put(k, k)
+	}
+
+	exp := "20"
+	got := m.Get(exp)
+	if exp != got {
+		t.Errorf("expectd: %s got: %v", exp, got)
+	}
+}
+
+func BenchmarkHash8(b *testing.B) {
+	m := NewMap()
+	for i := 0; i < b.N; i++ {
+		m.hash("12345678", 1024)
+	}
+}
+func BenchmarkHash16(b *testing.B) {
+	m := NewMap()
+	for i := 0; i < b.N; i++ {
+		m.hash("8888888888888888", 1024)
+	}
+}
+
+func BenchmarkGet(b *testing.B) {
+	m := NewMap()
+	size := 100
+	for i := 0; i < size; i++ {
+		k := fmt.Sprintf("%d", i)
+		m.Put(k, k)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m.Get(fmt.Sprintf("%d", i%size))
+	}
+}
